b3ce1f23a327a2b3b3f2e3d311e25fd5a7e2c1d0
YU-Kawasaki-05 <yuu.kw5.sea@gmail.com>
2026-04-15 14:03:22 +0900

    chore: configure quality tooling and baseline CI settings

---
+++ eslint.config.ts
@@ -0,0 +1,12 @@
+/** @file ESLint flat config for Next.js + TypeScript + Prettier. */
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypeScript from 'eslint-config-next/typescript'
+import prettier from 'eslint-config-prettier'
+
+export default defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypeScript,
+  prettier,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+])
